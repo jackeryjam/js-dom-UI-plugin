e0b02da4fdd1bbf0c8a820e55145ddf1a8ad25fe
jackeryjam <jackeryjam@gmail.com>
2018-04-07 20:45:11 +0800

    modify swipe and demo

--- demo/swipe/index.js
+++ demo/swipe/index.js
@@ -1,6 +1,6 @@
-var swipeview = require("js-dom-ui-plugin/dist/swipeview")
+var swipe = require("../../src/swipe")
 // swipeview有两个参数，一个是要滑动的部分的dom节点，这部分必须保证 后面可选部分是导航栏
-swipeview(document.getElementById("swipe-view"), {
-  activeClass: "nav-active",
-  navbar: document.getElementById("swipe-view-nav")
-});
+
+document.querySelectorAll(".item").forEach(item => swipe(item, {
+  clickCb: data => console.log(data)
+}))
--- src/swipe.js
+++ src/swipe.js
@@ -1,16 +1,15 @@
-module.exports = function (dom, clickCb, cb, option) {
+module.exports = function (dom, option) {
     var opt = {}
-    opt.domWidth = option && option.domWidth || window.innerWidth;
-    opt.appendWidth = option && option.appendWidth || 60;
+    var append = dom.querySelector('[swipe="append"]')
+    var appendWidth = append && append.offsetWidth || 60
     var currentPosition = 0; //记录当前页面位置
 
     var app = {
         //页面平移
         transform: function (translate) {
-            if (translate < -opt.appendWidth || translate > 0) return
+            if (translate < - appendWidth || translate > 0) return
             this.style.webkitTransform = "translate3d(" + translate + "px,0,0)";
             currentPosition = translate;
-
         },
 
         /**
@@ -19,7 +18,7 @@ module.exports = function (dom, clickCb, cb, option) {
         bindTouchEvent: function () {
             var viewport = dom;
             var pageWidth = window.innerWidth; //页面宽度
-            var maxWidth = - (opt.domWidth + opt.appendWidth);
+            var maxWidth = - (dom.offsetWidth + appendWidth);
             var startX, startY;
             var initialPos = 0;  // 手指按下的屏幕位置
             var moveLength = 0;  // 手指当前滑动的距离
@@ -32,58 +31,50 @@ module.exports = function (dom, clickCb, cb, option) {
 
             dom.addEventListener("touchstart", function (e) {
                 click = true
-                setTimeout(function(){
+                setTimeout(function () {
                     click = false
                 }, 300)
+
+                if (e.touches.length == 1 || isTouchEnd) {
+                    var touch = e.touches[0];
+                    startX = touch.pageX;
+                    startY = touch.pageY;
+                    initialPos = currentPosition;   //本次滑动前的初始位置
+                    viewport.style.webkitTransition = ""; //取消动画效果
+                    startT = new Date().getTime(); //记录手指按下的开始时间
+                    isMove = false; //是否产生滑动
+                    isTouchEnd = false; //当前滑动开始
+                }
             }.bind(this), false)
 
             /*手指在屏幕上滑动，页面跟随手指移动*/
             dom.addEventListener("touchmove", function (e) {
-                if (first) {
-                    first = false
-                    if (e.touches.length == 1 || isTouchEnd) {
-                        var touch = e.touches[0];
-                        startX = touch.pageX;
-                        startY = touch.pageY;
-                        initialPos = currentPosition;   //本次滑动前的初始位置
-                        viewport.style.webkitTransition = ""; //取消动画效果
-                        startT = new Date().getTime(); //记录手指按下的开始时间
-                        isMove = false; //是否产生滑动
-                        isTouchEnd = false; //当前滑动开始
+                e.preventDefault();
+                //如果当前滑动已结束，不管其他手指是否在屏幕上都禁止该事件
+                if (isTouchEnd) return;
+                var touch = e.touches[0];
+                var deltaX = touch.pageX - startX;
+                var deltaY = touch.pageY - startY;
+                //如果X方向上的位移大于Y方向，则认为是左右滑动
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    moveLength = deltaX < appendWidth ? deltaX : appendWidth;
+                    var translate = initialPos + moveLength; //当前需要移动到的位置
+                    //如果translate>0 或 < maxWidth,则表示页面超出边界
+                    if (translate <= 0 && translate >= maxWidth) {
+                        this.transform.call(viewport, translate);
+                        isMove = true;
                     }
-                } else {
-
-                    e.preventDefault();
-
-                    //如果当前滑动已结束，不管其他手指是否在屏幕上都禁止该事件
-                    if (isTouchEnd) return;
-
-                    var touch = e.touches[0];
-                    var deltaX = touch.pageX - startX;
-                    var deltaY = touch.pageY - startY;
-                    //如果X方向上的位移大于Y方向，则认为是左右滑动
-                    if (Math.abs(deltaX) > Math.abs(deltaY)) {
-                        moveLength = deltaX < opt.appendWidth ? deltaX : opt.appendWidth;
-                        var translate = initialPos + moveLength; //当前需要移动到的位置
-                        //如果translate>0 或 < maxWidth,则表示页面超出边界
-                        if (translate <= 0 && translate >= maxWidth) {
-                            this.transform.call(viewport, translate);
-                            isMove = true;
-                        }
-                        direction = deltaX > 0 ? "right" : "left"; //判断手指滑动的方向
-                    }
-
+                    direction = deltaX > 0 ? "right" : "left"; //判断手指滑动的方向
                 }
             }.bind(this), false);
 
             dom.addEventListener("touchend", function (e) {
-                if(click && clickCb) clickCb()
-                // e.preventDefault();
+                if (click && option && option.clickCb) option.clickCb(this.dataset)
                 //发生了滑动，并且当前滑动事件未结束
                 if (isMove && !isTouchEnd) {
                     isTouchEnd = true;
                     viewport.style.webkitTransition = "0.3s ease -webkit-transform";
-                    currentPosition = currentPosition > -opt.appendWidth / 2 ? 0 : - opt.appendWidth;
+                    currentPosition = currentPosition > - appendWidth / 2 ? 0 : - appendWidth;
                     app.transform.call(viewport, currentPosition);
                 }
                 first = true
