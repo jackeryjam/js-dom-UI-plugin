a860f60003f11233e5e767593de1ac371a50b78b
v_jackqzhan(詹前广) <v_jackqzhan@tencent.com>
2018-04-16 15:25:21 +0800

    preview fixtable

--- demo/fixable-top/index.js
+++ demo/fixable-top/index.js
@@ -1,17 +1,10 @@
 // var swipeview = require("js-dom-ui-plugin/dist/swipeview")
-var swipeview = require("@tencent/js-dom-ui-plugin/dist/swipeview")
-var fixable = require("@tencent/js-dom-ui-plugin/dist/fixable-top")
+var swipeview = require("../../src/swipeview")
+var fixable = require("../../src/fixable-top")
 // swipeview有两个参数，一个是要滑动的部分的dom节点，这部分必须保证 后面可选部分是导航栏
 swipeview(document.getElementById("swipe-view"), {
   activeClass: "nav-active",
   navbar: document.getElementById("swipe-view-nav")
 });
 
-fixable("id",{
-  toFixed: function(data){
-    console.log("toFixed")
-  },
-  toNormal: function(data){
-    console.log("toNormal")
-  }
-})
+fixable("id")
